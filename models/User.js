e13125efa9ef55a00b303f5e66ed5bbf78250340
Juan Carlos <jcarlosriego@gmail.com>
2020-02-17 11:25:05 +0100

    Added user model

--- models/User.js
+++ models/User.js
@@ -2,9 +2,27 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
+  username: {
+    type: String,
+    unique:true,
+    required: true
+  },
+  password: String,
+  name: String,
+  avatar: {
+    type: String,
+    default: `../images/default.png`
+  },
+  description: String,
+  avarageValue: {
+    tipe: Number,
+    default: `Sin experiencias previas.`
+  },
+  languages: {
+    type: String,
+    enum: [`español`, `inglés`, `portugués`, `francés`, `alemán`, `italiano`, `otro`],
+
+  },
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
