1803833b763f4ad9436920fa93f2d25a5612d69a
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-02-19 12:49:38 +0100

    form signup ok

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const User = require("../models/User");
 const Offer = require("../models/Offer");
+const uploadCloud = require('../config/cloudinary.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
