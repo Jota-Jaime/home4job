8cb501b394527152cf9a1a9a580363402c5c541b
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-02-20 16:04:37 +0100

    merge after luch

--- routes/offer.js
+++ routes/offer.js
@@ -5,12 +5,19 @@ const User = require("../models/User");
 const Offer = require("../models/Offer");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+const nodemailer = require("nodemailer")
 
 router.get("/all", (req, res, next) => {
   let user=req.user
   Offer.find().then((allOffers) => {
     res.render("offer/home", { allOffers, user});
     // res.json(user)
+  })
+});
+
+router.get("/contact", (req, res, next) => {
+  Offer.find().then(() => {
+    res.render("offer/contact");
   })
 });
 
@@ -56,5 +63,29 @@ router.get('/', (req,res,next) => {
   });
 });
 
+
+router.post('/contact', (req, res, next) => {
+  console.log("hola")
+ 
+  let { email, subject, message } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'hackmerla@gmail.com',
+      pass: 'ironhack1'
+    }
+  });
+
+
+  transporter.sendMail({
+    from: '"My Awesome Project " <myawesome@project.com>',
+    to: "hackmerla@gmail.com", 
+    subject: `Home4Job - Tienes una nueva solicitud `, 
+    text: message,
+    html: `<b>${message}</b>`
+  })
+  .then(info => res.redirect('/offer/all'))
+  .catch(error => console.log(error));
+});
 
 module.exports = router;
